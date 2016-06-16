8d727b95712b431351c2390d81144438c467482e
久保出雅俊 <kubode@team-lab.com>
2016-06-16 16:40:15 +0900

    Fix KDoc.

--- rxproperty-kotlin/src/main/kotlin/com/github/kubode/rxproperty/RxProperties.kt
+++ rxproperty-kotlin/src/main/kotlin/com/github/kubode/rxproperty/RxProperties.kt
@@ -1,33 +1,69 @@
-/**
- * Classes in this module overrides `value` property to provide null safety access for Kotlin.
- */
 package com.github.kubode.rxproperty
 
+/**
+ * Overrides `value` property to provide null safety access for Kotlin.
+ * @see ReadOnlyRxProperty
+ */
 class ReadOnlyRxPropertyKt<T>(state: ReadOnlyRxProperty.State<T>) : ReadOnlyRxProperty<T>(state) {
     override fun getValue(): T = super.getValue()
 }
 
+/**
+ * Overrides `value` property to provide null safety access for Kotlin.
+ * @see Constant
+ */
 class ConstantKt<T>(value: T) : Constant<T>(value) {
     override fun getValue(): T = super.getValue()
 }
 
+/**
+ * Overrides `value` property to provide null safety access for Kotlin.
+ * @see RxProperty
+ */
 class RxPropertyKt<T>(state: RxProperty.State<T>) : RxProperty<T>(state) {
     override fun setValue(value: T) = super.setValue(value)
     override fun getValue(): T = super.getValue()
 }
 
+/**
+ * Overrides `value` property to provide null safety access for Kotlin.
+ * @see Variable
+ */
 class VariableKt<T>(defaultValue: T) : Variable<T>(defaultValue) {
     override fun setValue(value: T) = super.setValue(value)
     override fun getValue(): T = super.getValue()
 }
 
+/**
+ * Overrides `value` property to provide null safety access for Kotlin.
+ * @see EmptyVariable
+ */
 class EmptyVariableKt<T>() : EmptyVariable<T>() {
     override fun setValue(value: T) = super.setValue(value)
     override fun getValue(): T = super.getValue()
 }
 
+/**
+ * @see ReadOnlyRxPropertyKt
+ */
 fun <T> readOnlyRxProperty(state: ReadOnlyRxProperty.State<T>) = ReadOnlyRxPropertyKt(state)
+
+/**
+ * @see ConstantKt
+ */
 fun <T> constant(value: T) = ConstantKt(value)
+
+/**
+ * @see RxPropertyKt
+ */
 fun <T> rxProperty(state: RxProperty.State<T>) = RxPropertyKt(state)
+
+/**
+ * @see VariableKt
+ */
 fun <T> variable(defaultValue: T) = VariableKt(defaultValue)
+
+/**
+ * @see EmptyVariableKt
+ */
 fun <T> emptyVariable() = EmptyVariableKt<T>()
